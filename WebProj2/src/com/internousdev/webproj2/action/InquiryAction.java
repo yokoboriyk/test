c855a60246331fcc678eec338ec3a15831ad5f30
yokoboriyk <g0ldenslumber@softbank.ne.jp>
2019-02-08 15:55:54 +0900

    2/8

---
+++ WebProj2/src/com/internousdev/webproj2/action/InquiryAction.java
@@ -0,0 +1,11 @@
+package com.internousdev.webproj2.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class InquiryAction extends ActionSupport{
+
+	public String excute(){
+		return SUCCESS;
+	}
+
+}
